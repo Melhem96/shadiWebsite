e9414185118e861fc5184a6f8f27005a5432ae94
Mohamad Melhem <mhmul96@google.com>
2026-02-15 17:57:34 +0100

    f

--- store-onepager/src/vite-env.d.ts
+++ store-onepager/src/vite-env.d.ts
@@ -1,2 +1,18 @@
-/// <reference types="vite/client" />
+declare interface ImportMetaEnv {
+  readonly VITE_SITE_NAME?: string;
+  readonly VITE_EMAIL_PUBLIC?: string;
+  readonly VITE_PHONE_E164?: string;
+  readonly VITE_PHONE_PRETTY?: string;
+  readonly VITE_ADDRESS_LINE?: string;
+  readonly VITE_GMAPS_Q?: string;
+  readonly VITE_GMAPS_EMBED?: string;
+  readonly VITE_HOURS_MON_FRI?: string;
+  readonly VITE_HOURS_SAT?: string;
+  readonly VITE_HOURS_SUN?: string;
+  readonly VITE_GOOGLE_SHEET_CONTACT_URL?: string;
+  readonly VITE_INSTAGRAM_HANDLE?: string;
+}
 
+declare interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
